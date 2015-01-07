268e23a49a34ecee62eb334266992381f82ce789
Steve Quinn <steve.m.quinn@gmail.com>
2015-01-07 11:01:34 +1100

    Added general configuration script for new OS installs.

---
+++ general_conf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set to debug mode to view output when executing
+set -x
+
+#
+# Git configuration
+#
+git config --global user.name "Steve Quinn"
+git config --global user.email steve.m.quinn@gmail.com
+git config --global core.editor vim
+
